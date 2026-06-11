6154f89f4f3657173ffc27af6abd32115f465ba5
Arpan Kaushal <arpankaushal@192.168.1.6>
2026-06-11 16:17:28 +0530

    internal link extraction

--- backend/crawler/crawler.py
+++ backend/crawler/crawler.py
@@ -1,29 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
+
 
 def get_links(url):
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, "html.parser")
 
-    links = []
+    links = set()
+
+    base_domain = urlparse(url).netloc
 
     for a in soup.find_all("a"):
         href = a.get("href")
 
         if href:
-    
+
             if href.startswith("#"):
                 continue
+
             if href.startswith("javascript:"):
                 continue
 
             if href.startswith("mailto:"):
                 continue
-            
-            
-            full_url = urljoin(url, href)
-            links.append(full_url)
 
-    return links
+            full_url = urljoin(url, href)
+
+            link_domain = urlparse(full_url).netloc
+
+            if link_domain == base_domain:
+                links.add(full_url)
+
+    return list(links)
