5427db30c6b70ea464214bb89ebb617f3e04b46e
Arpan Kaushal <arpankaushal@192.168.1.32>
2026-06-19 17:23:21 +0530

    fixed apibase

--- backend/api.py
+++ backend/api.py
@@ -11,13 +11,16 @@ app = FastAPI(
 )
 
 origins = [
+    "http://localhost:5173",
     "https://brochuregenerator47.vercel.app"
 ]
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],      
+    allow_origins=[
+    "http://localhost:5173",
+    "https://brochuregenerator47.vercel.app"],      
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
